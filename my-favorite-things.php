e3aed34fb13874b181cf27f538ac614029b0c9fe
Mauro Cabrales <maurocabrales@Macs-MacBook-Pro-2.local>
2016-05-05 14:13:08 -0500

    this exercise used a foreach loop in php to display an array

---
+++ my-favorite-things.php
@@ -0,0 +1,18 @@
+<?php  
+
+
+
+
+
+
+
+
+$favoriteThings = ["Off-Road", "Motorcycle Travel", "Kayaking", "Fishing", "Movies"];
+
+
+foreach ($favoriteThings as $value) {
+
+    echo "My favorite things are {$value}\n";
+
+}
+
